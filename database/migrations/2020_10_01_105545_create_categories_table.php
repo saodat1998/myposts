70dd464357f8b3f8148ad5550d7159e0d0194410
saodat <saodat.9808@gmail.com>
2020-10-18 21:41:41 +0500

    migrations

--- database/migrations/2020_10_01_105545_create_categories_table.php
+++ database/migrations/2020_10_01_105545_create_categories_table.php
@@ -15,7 +15,9 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('name')->index();
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->string('status')->default('ACTIVE');
             $table->timestamps();
         });
     }
--- database/migrations/2020_10_02_185611_create_posts_table.php
+++ database/migrations/2020_10_02_185611_create_posts_table.php
@@ -16,10 +16,13 @@ class CreatePostsTable extends Migration
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title');
-            $table->mediumText('body');
+            $table->mediumText('body')->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('status')->default('DRAFT');
             $table->timestamps();
-
-
         });
     }
 
